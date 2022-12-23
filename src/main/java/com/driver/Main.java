4898c2df5cf2bfa40c515a03c66b2db35788bfb2
Abhinaba Roy Choudhury <abhi.roychoudhury.8@gmail.com>
2022-12-24 01:51:38 +0530

    hogaya

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        //obj.name = "nice";
+        //'name' has private access in 'com.driver.RWOnly'
+
+        obj.setName("Abhi");
+        System.out.println(obj.getName());
+    }
 }
